550c59edc7bc76099c97b5d0798edb1a65e89484
bloomingkiwi <shihoritsuji@ShihorinoMacBook-Air.local>
2022-10-04 15:31:51 +0900

    primarykeyの削除、カラムの追加

--- database/migrations/2022_10_04_024144_create_conversations_table.php
+++ database/migrations/2022_10_04_024144_create_conversations_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
     public function up()
     {
         Schema::create('conversations', function (Blueprint $table) {
-            $table->id()->primary();
+            $table->id();
             $table->string('name');
             $table->integer('sending_user_id');
             $table->integer('group_id');
