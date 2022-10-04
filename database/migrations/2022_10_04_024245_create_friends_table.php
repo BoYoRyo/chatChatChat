13dd24fecd1bbb8efa8da133805bafab80653ac3
bloomingkiwi <shihoritsuji@ShihorinoMacBook-Air.local>
2022-10-04 12:12:50 +0900

    友達テーブルのスキーマを拡張

--- database/migrations/2022_10_04_024245_create_friends_table.php
+++ database/migrations/2022_10_04_024245_create_friends_table.php
@@ -15,6 +15,9 @@ return new class extends Migration
     {
         Schema::create('friends', function (Blueprint $table) {
             $table->id();
+            $table->integer('user_id');
+            $table->integer('follow_id');
+            $table->boolean('blocked');
             $table->timestamps();
         });
     }
