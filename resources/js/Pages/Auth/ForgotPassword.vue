c0d222576f42cc13fd0d93e54a61ce656ba50503
shiibashi-java2022 <ryouta0919.0919@gmail.com>
2022-10-06 17:15:02 +0900

    ログイン画面、登録画面、ダッシュボードの日本語化

--- resources/js/Pages/Auth/ForgotPassword.vue
+++ resources/js/Pages/Auth/ForgotPassword.vue
@@ -24,7 +24,7 @@ const submit = () => {
         <Head title="Forgot Password" />
 
         <div class="mb-4 text-sm text-gray-600">
-            Forgot your password? No problem. Just let us know your email address and we will email you a password reset link that will allow you to choose a new one.
+            パスワードをお忘れですか？登録済みのメールアドレスに新しいパスワードを設定できるリンクをメールでお送りします。
         </div>
 
         <div v-if="status" class="mb-4 font-medium text-sm text-green-600">
@@ -33,14 +33,14 @@ const submit = () => {
 
         <form @submit.prevent="submit">
             <div>
-                <InputLabel for="email" value="Email" />
+                <InputLabel for="email" value="メースアドレス" />
                 <TextInput id="email" type="email" class="mt-1 block w-full" v-model="form.email" required autofocus autocomplete="username" />
                 <InputError class="mt-2" :message="form.errors.email" />
             </div>
 
             <div class="flex items-center justify-end mt-4">
                 <PrimaryButton :class="{ 'opacity-25': form.processing }" :disabled="form.processing">
-                    Email Password Reset Link
+                    メールを送信する
                 </PrimaryButton>
             </div>
         </form>
